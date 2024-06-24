41e922745af85b3425805dda78c0f5758e6d8a69
Renato Lima <renatoaugustoml@gmail.com>
2024-06-24 21:15:33 +0200

    chore(api): fastify setup

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@next-saas-rbac/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER' })
-
-const userCanInviteSomeone = ability.can('invite', 'User')
-const userCanDeleteSomeone = ability.can('delete', 'User')
-
-console.log('userCanInviteSomeone', userCanInviteSomeone)
-console.log('userCanDeleteSomeone', userCanDeleteSomeone)
