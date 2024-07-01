5482cd58082bd4ce8018eb95a3617f196230434c
Renato Lima <renatoaugustoml@gmail.com>
2024-07-01 20:09:54 +0200

    feat(api): add create user  route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,21 +1,48 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+const schema = {
+  body: z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  }),
+}
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
     {
-      schema: {
-        body: z.object({
-          name: z.string(),
-          email: z.string().email(),
-          password: z.string().min(6),
-        }),
-      },
+      schema,
     },
-    () => {
-      return 'User created!'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userAlreadyExists = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userAlreadyExists) {
+        return reply
+          .status(400)
+          .send({ message: 'A user with this email already exists!' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
