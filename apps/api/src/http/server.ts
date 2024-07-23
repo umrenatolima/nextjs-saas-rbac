c460fc8abc2b04734cdb1c102eafcc692584f4e4
Renato Lima <renatoaugustoml@gmail.com>
2024-07-23 07:38:00 +0200

    refactor(api): change swagger docs route auth

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -33,7 +33,15 @@ app.register(fastifySwagger, {
       description: 'Fullstack SaaS app with multi-tenant & RBAC',
       version: '1.0.0',
     },
-    servers: [],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
   },
   transform: jsonSchemaTransform,
 })
