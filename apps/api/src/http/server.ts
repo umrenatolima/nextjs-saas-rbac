c4bbd498319d91dda4f2c9edbd25456fdc0fe20a
Renato Lima <renatoaugustoml@gmail.com>
2024-07-24 23:48:31 +0200

    feat(routes): add route to get organization details
    Implemented a new route to get organization details by slug. This includes:
    
    - Endpoint: GET /organizations/:slug
    - Validation with zod for slug in params
    - Uses auth middleware to authenticate user and get organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { resetPassword } from './routes/auth/reset-password'
 import { sendPasswordResetEmail } from './routes/auth/send-password-reset-email'
 import { createOrganization } from './routes/orgs/create-organization'
+import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -64,6 +65,7 @@ app.register(createAccount)
 app.register(getProfile)
 app.register(resetPassword)
 app.register(sendPasswordResetEmail)
+app.register(getOrganization)
 app.register(getOrganizations)
 
 app.register(createOrganization)
